5f320397db6608f884eb7b6fa86406b4b657eb2c
Robert Landrum <rlandrum@verisign.com>
2013-10-22 21:52:03 +0000

    Added schema CRUD

--- sis.js
+++ sis.js
@@ -1,6 +1,9 @@
 angular.module('sis',[]).config(function($routeProvider) {
     $routeProvider.
         when('/',{controller:SchemaListCtrl, templateUrl:'schema_list.html'}).
+        when('/create',{controller:SchemaCreateCtrl, templateUrl: 'schema_create.html'}).
+        when('/edit/:schema',{controller:SchemaEditCtrl,templateUrl:'schema_edit.html'}).
+        when('/remove/:schema',{controller:SchemaRemoveCtrl,templateUrl:'schema_remove.html'}).
         when('/list/:schema',{controller:EntityListCtrl, templateUrl:'entity_list.html'}).
         when('/edit/:schema/:id',{controller:EntityEditCtrl, templateUrl:'entity_edit.html'}).
         when('/remove/:schema/:id',{controller:EntityRemoveCtrl, templateUrl: 'entity_remove.html'}).
@@ -8,11 +11,22 @@ angular.module('sis',[]).config(function($routeProvider) {
         otherwise({redirectTo:'/'});
 });
 
+function notify(msg) {
+    var not = $('<div class="alert alert-success">'+msg+'</div>');
+    $('#notification_area').append(not);
+    not.fadeOut(3500,function() {
+        not.remove();
+    });
+}
+
 function SchemaListCtrl($scope,$http) {
+    $scope.record_count = 0;
     $scope.refresh = function() {
         $http.get("http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/schemas")
         .success(function(data) {
             $scope.schemas = data;
+            $scope.record_count = data.length;
+
         })
         .error(function(data,status) {
             alert("Unable to fetch data from SIS: "+data+":"+status);
@@ -22,6 +36,93 @@ function SchemaListCtrl($scope,$http) {
 }
 
 function SchemaEditCtrl($scope,$http,$routeParams) {
+    $scope.schema = $routeParams.schema;
+    $scope.onload = function() {
+        $http.get("http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/schemas/"+$scope.schema)
+        .success(function(data) {
+            $scope.doc = data;
+            $scope.doc.definition = JSON.stringify(data.definition);
+        })
+        .error(function(data,status) {
+            alert("Unable to fetch data from SIS: "+data+":"+status);
+        });
+    }
+    $scope.save = function() {
+        delete $scope.doc._id;
+        delete $scope.doc.__v;
+        $scope.definition = JSON.parse($scope.definition);
+        $http({
+            "method": "PUT",
+            "url": "http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/schemas/"+$scope.schema,
+            "data": JSON.stringify($scope.doc),
+            "headers": {
+                "Content-type": "application/json"
+            },
+        })
+        .success(function(data) {
+            notify('This schema has been successfully updated.');
+            $location.path('/');
+        })
+        .error(function(data,status) {
+            alert("Failed to update data in SIS: "+JSON.stringify($scope.doc)+": "+status);
+        });
+    }
+    $scope.back = function() {
+        window.history.back();
+    }
+    $scope.onload();
+    
+}
+function SchemaCreateCtrl($scope,$http,$location) {
+
+    // Grab existing data, and pop open a modal with a form
+    // ----------------------------------------------------
+    $scope.onload = function() {
+        $scope.doc = {
+            "name" : '',
+            "owner" : '',
+            "definition" : '',
+        };
+    };
+    $scope.create = function() {
+        $scope.definition = JSON.parse($scope.definition);
+        $http({
+            "method": "POST",
+            "url": "http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/schemas/",
+            "data": JSON.stringify($scope.doc),
+            "headers": {
+                "Content-type": "application/json"
+            },
+        })
+        .success(function(data) {
+            notify('This schema has been successfully created.');
+            $location.path('/');
+        })
+        .error(function(data,status) {
+            alert("Failed to update data in SIS: "+JSON.stringify($scope.doc)+": "+status);
+        });
+    }
+    $scope.back = function() {
+        window.history.back();
+    }
+    $scope.onload();
+}
+function SchemaRemoveCtrl($scope,$http,$routeParams,$location) {
+    $scope.schema = $routeParams.schema;
+
+    $scope.remove_schema = function() {
+        $http.delete("http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/schemas/"+$scope.schema)
+        .success(function(data) {
+            notify('This schema has been successfully removed.');
+            $location.path('/');
+        })
+        .error(function(data,status) {
+            alert("Unable to fetch data from SIS: "+data+":"+status);
+        });
+    }
+    $scope.back = function() {
+        window.history.back();
+    }
 }
 
 function EntityListCtrl($scope,$http,$routeParams) {
@@ -49,6 +150,9 @@ function EntityListCtrl($scope,$http,$routeParams) {
         });
 
     };
+    $scope.back = function() {
+        window.history.back();
+    }
     $scope.refresh();
 }
 function EntityEditCtrl($scope,$http,$routeParams,$location) {
@@ -91,16 +195,15 @@ function EntityEditCtrl($scope,$http,$routeParams,$location) {
             },
         })
         .success(function(data) {
-            var not = $('<div class="alert alert-success">This entity has been successfully updated.</div>');
-            $('#notification_area').append(not);
-            not.fadeOut(2500,function() {
-                not.remove();
-            });
+            notify('This entity has been successfully updated.');
             $location.path('/list/'+$scope.schema);
         })
         .error(function(data,status) {
             alert("Failed to update data in SIS: "+JSON.stringify($scope.doc)+": "+status);
         });
+    }
+    $scope.back = function() {
+        window.history.back();
     }
     $scope.onload();
 }
@@ -136,16 +239,15 @@ function EntityCreateCtrl($scope,$http,$routeParams,$location) {
             },
         })
         .success(function(data) {
-            var not = $('<div class="alert alert-success">This entity has been successfully created.</div>');
-            $('#notification_area').append(not);
-            not.fadeOut(2500,function() {
-                not.remove();
-            });
+            notify('This entity has been successfully created.');
             $location.path('/list/'+$scope.schema);
         })
         .error(function(data,status) {
             alert("Failed to update data in SIS: "+JSON.stringify($scope.doc)+": "+status);
         });
+    }
+    $scope.back = function() {
+        window.history.back();
     }
     $scope.onload();
 }
@@ -157,11 +259,7 @@ function EntityRemoveCtrl($scope,$http,$routeParams,$location) {
     $scope.remove_entity = function() {
         $http.delete("http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/entities/"+$scope.schema+'/'+$scope.id)
         .success(function(data) {
-            var not = $('<div class="alert alert-success">Entity has been successfully removed.</div>');
-            $('#notification_area').append(not);
-            not.fadeOut(2500,function() {
-                not.remove();
-            });
+            notify('Entity has been successfully removed.');
             $location.path('/list/'+$scope.schema);
         })
         .error(function(data,status) {
