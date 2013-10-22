f1d87aa2acf59838e56ad2708eb39015354bf737
Robert Landrum <rlandrum@verisign.com>
2013-10-22 20:36:40 +0000

    Modifications to allow for entity crud

--- sis.js
+++ sis.js
@@ -1,8 +1,10 @@
 angular.module('sis',[]).config(function($routeProvider) {
     $routeProvider.
         when('/',{controller:SchemaListCtrl, templateUrl:'schema_list.html'}).
-        when('/show/:schema',{controller:EntityListCtrl, templateUrl:'entity_list.html'}).
+        when('/list/:schema',{controller:EntityListCtrl, templateUrl:'entity_list.html'}).
         when('/edit/:schema/:id',{controller:EntityEditCtrl, templateUrl:'entity_edit.html'}).
+        when('/remove/:schema/:id',{controller:EntityRemoveCtrl, templateUrl: 'entity_remove.html'}).
+        when('/create/:schema',{controller:EntityCreateCtrl, templateUrl:'entity_create.html'}).
         otherwise({redirectTo:'/'});
 });
 
@@ -89,7 +91,12 @@ function EntityEditCtrl($scope,$http,$routeParams,$location) {
             },
         })
         .success(function(data) {
-            $location.path('/show/'+$scope.schema);
+            var not = $('<div class="alert alert-success">This entity has been successfully updated.</div>');
+            $('#notification_area').append(not);
+            not.fadeOut(2500,function() {
+                not.remove();
+            });
+            $location.path('/list/'+$scope.schema);
         })
         .error(function(data,status) {
             alert("Failed to update data in SIS: "+JSON.stringify($scope.doc)+": "+status);
@@ -97,3 +104,72 @@ function EntityEditCtrl($scope,$http,$routeParams,$location) {
     }
     $scope.onload();
 }
+function EntityCreateCtrl($scope,$http,$routeParams,$location) {
+    $scope.schema = $routeParams.schema;
+
+    // Grab existing data, and pop open a modal with a form
+    // ----------------------------------------------------
+    $scope.onload = function() {
+        // First get schema definition so we can display column names
+        // ----------------------------------------------------------
+        $http.get("http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/schemas/"+$scope.schema)
+        .success(function(data) {
+            $scope.defkeys = [];
+            $scope.doc = {};
+            for(var k in data.definition) {
+              $scope.defkeys.push(k);
+              $scope.doc[k] = '';
+            }
+        })
+        .error(function(data,status) {
+            alert("Unable to fetch data from SIS: "+data+":"+status);
+        });
+
+    };
+    $scope.create = function() {
+        $http({
+            "method": "POST",
+            "url": "http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/entities/"+$scope.schema,
+            "data": JSON.stringify($scope.doc),
+            "headers": {
+                "Content-type": "application/json"
+            },
+        })
+        .success(function(data) {
+            var not = $('<div class="alert alert-success">This entity has been successfully created.</div>');
+            $('#notification_area').append(not);
+            not.fadeOut(2500,function() {
+                not.remove();
+            });
+            $location.path('/list/'+$scope.schema);
+        })
+        .error(function(data,status) {
+            alert("Failed to update data in SIS: "+JSON.stringify($scope.doc)+": "+status);
+        });
+    }
+    $scope.onload();
+}
+
+function EntityRemoveCtrl($scope,$http,$routeParams,$location) {
+    $scope.id = $routeParams.id;
+    $scope.schema = $routeParams.schema;
+
+    $scope.remove_entity = function() {
+        $http.delete("http://sis-node1.dev-bo.iad1.vrsn.com/api/v1/entities/"+$scope.schema+'/'+$scope.id)
+        .success(function(data) {
+            var not = $('<div class="alert alert-success">Entity has been successfully removed.</div>');
+            $('#notification_area').append(not);
+            not.fadeOut(2500,function() {
+                not.remove();
+            });
+            $location.path('/list/'+$scope.schema);
+        })
+        .error(function(data,status) {
+            alert("Unable to fetch data from SIS: "+data+":"+status);
+        });
+    }
+    $scope.back = function() {
+        window.history.back();
+    }
+}
+
